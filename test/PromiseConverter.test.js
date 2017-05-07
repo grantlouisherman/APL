047d30871c44ce848f23f6b191d944a0e0a1cf54
gr <grantlouisherman041@gmail.com>
2017-05-07 13:34:48 -0400

    adding a few tests for fromFunctionToPromise

--- test/PromiseConverter.test.js
+++ test/PromiseConverter.test.js
@@ -8,7 +8,7 @@ describe('Testing fromFunctionToPromise', function () {
 		classInvocation = new PromiseConverter();
 		testFnParameters = function (a,b) {
 			let c = 5;
-			return (a*b)^c;
+			return (a*b)*c;
 		}
 		testFnNoParameters = function () {
 			return "TEST TEST"
@@ -16,6 +16,27 @@ describe('Testing fromFunctionToPromise', function () {
 	})
 	it('fromFunctionToPromise returns a promise object', function () {
   		expect(typeof classInvocation.fromFunctionToPromise(testFnNoParameters())).to.equal("object");
+    });
+
+    it('fromFunctionToPromise returns the proper value with parameters passed in', function () {
+    	classInvocation.fromFunctionToPromise(testFnParameters(2,3)).then(function (value) {
+  			expect(value).to.equal(30)
+  		})
+  		
+    });
+
+     it('fromFunctionToPromise returns the proper value with no parameters passed in', function () {
+    	classInvocation.fromFunctionToPromise(testFnNoParameters()).then(function (value) {
+  			expect(value).to.equal("TEST TEST")
+  		})
+  		
+    });
+
+      it('fromFunctionToPromise returns the function with the proper value without it being invoked', function () {
+    	classInvocation.fromFunctionToPromise(testFnNoParameters).then(function (value) {
+  			expect(value).to.equal("TEST TEST")
+  		})
+  		
     });
 
 })
